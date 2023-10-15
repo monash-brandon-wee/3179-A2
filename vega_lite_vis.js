4d9304a178b6f3b10e79ea61ee6be9d730f6e9c5
monash-brandon-wee <126179133+monash-brandon-wee@users.noreply.github.com>
2023-10-15 22:48:26 +0800

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,23 @@
+var vg_1 = "vis1.json";
+
+vegaEmbed("#bubble_chart", vg_1, {"actions": false}).then(console.log).catch(console.warn);
+
+var vg_2 = "vis2.json";
+
+vegaEmbed("#scatter_plot", vg_2, {"actions": false}).then(console.log).catch(console.warn);
+
+var vg_3 = "vis3.json";
+
+vegaEmbed("#line_chart", vg_3, {"actions": false}).then(console.log).catch(console.warn);
+
+var vg_4 = "vis4.json";
+
+vegaEmbed("#donut_chart", vg_4, {"actions": false}).then(console.log).catch(console.warn);
+
+var vg_5 = "vis5.json";
+
+vegaEmbed("#map", vg_5, {"actions": false}).then(console.log).catch(console.warn);
+
+var vg_6 = "vis6.json";
+
+vegaEmbed("#lollipop_chart", vg_6, {"actions": false}).then(console.log).catch(console.warn);
